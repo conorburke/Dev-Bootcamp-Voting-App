99737ba968373cbbd3e75559bf058da075f75a7e
Tae Yun <yuntjs@gmail.com>
2016-12-08 14:03:48 -0800

    configure controller and view of ideas to make button green when selected, which is kept in the session

--- config/routes.rb
+++ config/routes.rb
@@ -4,6 +4,10 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   root :to => "home#index"
 
+  get '/session-viewer' => "sessions#view_session"
+
+  get '/ideas/select/:id' => "ideas#add_sessions"
+
   get '/ideas' => "ideas#index"
   get '/ideas/new' => "ideas#new"
   post '/ideas' => "ideas#create"
