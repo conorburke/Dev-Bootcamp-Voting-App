bc7ad7c98d48ce1d4ee31c22e73d7a436372aded
Qiaoshu Lin & Tae Yun <pair+chelseylin.yuntsj@devbootcamp.com>
2016-12-08 13:26:52 -0800

    student rspec passed

--- db/migrate/20161208202843_add_current_access_to_students.rb
+++ db/migrate/20161208202843_add_current_access_to_students.rb
@@ -1,5 +1,5 @@
 class AddCurrentAccessToStudents < ActiveRecord::Migration[5.0]
   def change
-    add_column :students, :current_access, :string, default: ""
+    add_column :students, :current_access, :string, default: nil
   end
 end
