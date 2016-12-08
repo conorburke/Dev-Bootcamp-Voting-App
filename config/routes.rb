7b75cba0b7e0bcc4d51b0ce778b0684dc5c65731
conorburke <cjburke89@gmail.com>
2016-12-07 17:06:52 -0800

    user can see ideas and create new ideas

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,8 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 
   get '/ideas' => "ideas#index"
+  get '/ideas/new' => "ideas#new"
+  post '/ideas' => "ideas#create"
   root 'ideas#index'
 
 end
