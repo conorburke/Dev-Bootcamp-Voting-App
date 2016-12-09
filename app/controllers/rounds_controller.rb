7b7ea0df5f4f9c8d66ac6b6970323a10f623e135
Qiaoshu Lin & Tae Yun <pair+chelseylin.yuntsj@devbootcamp.com>
2016-12-09 10:47:15 -0800

    implement voting-cut-method

--- app/controllers/rounds_controller.rb
+++ app/controllers/rounds_controller.rb
@@ -7,13 +7,17 @@ class RoundsController < ApplicationController
 
   def update
     @round = Round.find_by_id(params[:id])
+    # Disable Voting
     if @round.cohort.students.find { |student| student.current_access == "voting #{@round.voting_round}" }
       @round.cohort.students.each { |student| student.current_access = ""; student.save }
       active_ideas = @round.cohort.ideas.select { |idea| idea.active? }
-      active_ideas.each do |idea|
-        idea.update_attribute(:active, 0) unless @round.votes.find { |vote| vote.idea == idea }
+      ordered_ideas = active_ideas.sort_by { |idea| idea.vote_count(@round) }.reverse
+      limit = [4, (ordered_ideas.count / 2)].max
+      ordered_ideas[limit..-1].each do |idea|
+        idea.update_attribute(:active, 0)
       end
       redirect_to current_user
+    # Enable Voting
     else
       @round.cohort.students.each { |student| student.current_access = "voting #{@round.voting_round}"; student.save }
       redirect_to current_user
