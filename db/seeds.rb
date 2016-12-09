ee567e9a0ac3b994127206d2468ef592875b33cf
Qiaoshu Lin & Tae Yun <pair+chelseylin.yuntsj@devbootcamp.com>
2016-12-09 15:46:19 -0800

    group dropping function working

--- db/seeds.rb
+++ db/seeds.rb
@@ -7,12 +7,12 @@
 #   Character.create(name: 'Luke', movie: movies.first)
 require 'faker'
 
-# City.delete_all
-# Cohort.delete_all
-# Teacher.delete_all
-# Student.delete_all
-# Idea.delete_all
-# Group.delete_all
+City.delete_all
+Cohort.delete_all
+Teacher.delete_all
+Student.delete_all
+Idea.delete_all
+Group.delete_all
 
 city = City.create(name: "San Diego")
 cohort = Cohort.create(name: "chipmunks", city_id: city.id)
@@ -20,19 +20,19 @@ teacher = Teacher.create(first_name: "Kevin", last_name: "Solorio", email: "kevi
 student = Student.create(first_name: "John", last_name: "Smith", email: "jsmith@gmail.com", cohort_id: cohort.id)
 idea = Idea.create(title: "good idea", student_id: student.id)
 
-# idea1 = Idea.create(title: "good idea1", student_id: student.id)
-# idea2 = Idea.create(title: "good idea2", student_id: student.id)
-# idea3 = Idea.create(title: "good idea3", student_id: student.id)
-# idea4 = Idea.create(title: "good idea4", student_id: student.id)
-#
-# ideas = [idea,idea1,idea2,idea3,idea4]
-# groups = []
-# for i in 0...4
-#   groups << Group.create(name: Faker::Superhero.name, idea_id: ideas[i].id)
-# end
-# p groups
-# for i in 0...20
-#   puts i/5
-#   group = groups[i/5]
-#   group.students.create(first_name: Faker::Name.first_name, last_name: Faker::Name.last_name, email: Faker::Internet.email, cohort_id: cohort.id)
-# end
+idea1 = Idea.create(title: "good idea1", student_id: student.id)
+idea2 = Idea.create(title: "good idea2", student_id: student.id)
+idea3 = Idea.create(title: "good idea3", student_id: student.id)
+idea4 = Idea.create(title: "good idea4", student_id: student.id)
+
+ideas = [idea,idea1,idea2,idea3,idea4]
+groups = []
+for i in 0...4
+  groups << Group.create(name: Faker::Superhero.name, idea_id: ideas[i].id)
+end
+p groups
+for i in 0...20
+  puts i/5
+  group = groups[i/5]
+  group.students.create(first_name: Faker::Name.first_name, last_name: Faker::Name.last_name, email: Faker::Internet.email, cohort_id: cohort.id)
+end
